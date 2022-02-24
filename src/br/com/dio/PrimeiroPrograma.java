5c227f8e9d333fa0336396188865989eb55957af
marlonricardo <mroparaty@gmail.com>
2022-02-23 23:22:32 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        //System.out.println("Hello world" + (7+2));
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+}
